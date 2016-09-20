30fa6c4e687af17cdadb1191fac0b31b8007ee9a
Michael Loschiavo <losch103@gmail.com>
2016-09-20 10:02:36 -0700

    completed Board#make_move. Bug on next_turn logic

--- lib/board.rb
+++ lib/board.rb
@@ -32,29 +32,61 @@ class Board
     @cups[start_pos] = []
     index = start_pos + 1
     until stones.empty?
-    index -= 13 if index > 13
-    index += 1 if (current_player_name == @name1 && index == 13 || current_player_name == @name2 && index == 6)
 
-      @cups[index]. << stones.pop
-      index += 1
+    index += 1 if (current_player_name == @name1 && index == 13) || (current_player_name == @name2 && index == 6)
+
+    index -= 14 if index > 13
+
+      @cups[index] << stones.pop
+      index += 1 unless stones.empty?
     end
+    render
+    next_turn(index)
   end
 
   def next_turn(ending_cup_idx)
     # helper method to determine what #make_move returns
+    debugger
+    if @cups[ending_cup_idx].empty?
+      :switch
+    elsif (ending_cup_idx == 13) || (ending_cup_idx == 6)
+      :prompt
+    else
+    ending_cup_idx
+    end
   end
 
   def render
-    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
-    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
-    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
-    puts ""
-    puts ""
+    # print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    # puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    # print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    # puts ""
+    # puts ""
   end
 
   def cups_empty?
+    is_empty = true
+    (0..5).each do |i|
+      is_empty = false unless @cups[i].empty?
+    end
+    (7..12).each do |j|
+      is_empty = false unless @cups[j].empty?
+    end
+    is_empty
   end
 
   def winner
+    prc = Proc.new {|a,b| a <=> b}
+    p1_score = @cups[6].count
+    p2_score = @cups[13].count
+
+    case prc.call(p1_score,p2_score)
+    when -1
+      @name2
+    when 0
+      :draw
+    when 1
+      @name1
+    end
   end
 end
