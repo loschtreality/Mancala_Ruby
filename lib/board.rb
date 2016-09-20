8c85a5097dcff28c83d97a551d981c47a2ae8a36
Michael Loschiavo <losch103@gmail.com>
2016-09-19 22:41:29 -0700

    Bug at Board#make_move. Initialize and validation methods complete

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,43 @@
+require 'byebug'
+
 class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @name1, @name2 = name1, name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
   end
 
   def place_stones
-    # helper method to #initialize every non-store cup with four stones each
+    stones = [:stone, :stone, :stone, :stone]
+    @cups.each_with_index do |cup,idx|
+      unless (idx == 6 || idx == 13)
+        cup.concat(stones)
+      end
+    end
   end
 
   def valid_move?(start_pos)
+    if (start_pos > 12 || start_pos < 1)
+      raise "Invalid starting cup"
+    elsif (start_pos == 6 || start_pos == 12)
+      raise "Invalid starting cup"
+    end
+    true
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos].dup
+    @cups[start_pos] = []
+    index = start_pos + 1
+    until stones.empty?
+    index -= 13 if index > 13
+    index += 1 if (current_player_name == @name1 && index == 13 || current_player_name == @name2 && index == 6)
+
+      @cups[index]. << stones.pop
+      index += 1
+    end
   end
 
   def next_turn(ending_cup_idx)
